a91077b37199d12abbd4903a564f85746dc4eb97
nguyenhoanganhtuan1206 <nguyenhoanganhtuan1206@gmail.com>
2025-03-15 09:22:50 +0700

    Update migration

--- quizlet-be/src/main/resources/ini_data/migration/COMMON/V1_20250114__initial_schema.sql
+++ quizlet-be/src/main/resources/ini_data/migration/COMMON/V1_20250114__initial_schema.sql
@@ -1,23 +1,24 @@
-CREATE
-EXTENSION IF NOT EXISTS "uuid-ossp";
+-- Enable UUID extension if it doesn't exist
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
---- CREATE TABLE roles
+-- Create table: roles
 CREATE TABLE roles (
-    id SERIAL PRIMARY KEY,
-    name VARCHAR(50) NOT NULL UNIQUE
+    id          SERIAL PRIMARY KEY,
+    name        VARCHAR(50) NOT NULL UNIQUE
 );
 
---- CREATE TABLE USER
+-- Create table: users
 CREATE TABLE users (
-    id UUID PRIMARY KEY,
-    code_reset_password VARCHAR(255),
-    created_at TIMESTAMP NOT NULL DEFAULT NOW(),
-    email VARCHAR(255) NOT NULL UNIQUE,
+    id                          UUID PRIMARY KEY,
+    code_reset_password         VARCHAR(255),
+    created_at                  TIMESTAMP NOT NULL DEFAULT NOW(),
+    email                       VARCHAR(255) NOT NULL UNIQUE,
     last_send_reset_password_at TIMESTAMP,
-    password VARCHAR(255) NOT NULL,
-    account_disabled BOOLEAN NOT NULL DEFAULT FALSE,
-    full_name VARCHAR(100) NOT NULL,
-    role_id INTEGER NOT NULL,
+    password                    VARCHAR(255) NOT NULL,
+    account_disabled            BOOLEAN NOT NULL DEFAULT FALSE,
+    full_name                   VARCHAR(100) NOT NULL,
+    image                       VARCHAR(255),
+    role_id                     INTEGER NOT NULL,
     CONSTRAINT fk_role
         FOREIGN KEY (role_id)
         REFERENCES roles (id)
--- quizlet-be/src/main/resources/ini_data/migration/COMMON/V1_20250307__create_table_data_folders.sql
+++ quizlet-be/src/main/resources/ini_data/migration/COMMON/V1_20250307__create_table_data_folders.sql
@@ -1,21 +1,28 @@
-CREATE
-EXTENSION IF NOT EXISTS "uuid-ossp";
+-- Enable UUID extension if it doesn't exist
+CREATE EXTENSION IF NOT EXISTS "uuid-ossp";
 
--- Create table folders
+-- Create table: folders
 CREATE TABLE folders (
     id          UUID            PRIMARY KEY,
     name        VARCHAR(50)     NOT NULL        UNIQUE,
     description VARCHAR(50),
-    created_at  TIMESTAMP       NOT NULL        DEFAULT     NOW(),
+    created_at  TIMESTAMP       NOT NULL        DEFAULT NOW(),
     updated_at  TIMESTAMP,
     user_id     UUID            NOT NULL,
     CONSTRAINT  fk_user
         FOREIGN KEY (user_id)
         REFERENCES  users (id)
-        ON DELETE CASCADE
+        ON DELETE   CASCADE
 );
 
--- Insert data for Folders
-INSERT INTO folders (id, name, description, created_at, updated_at, user_id)
+-- Insert data for folders
+INSERT INTO folders (
+    id,
+    name,
+    description,
+    created_at,
+    updated_at,
+    user_id
+)
 VALUES
-    ('21a95216-b38d-483f-bb26-cc8cbdd9366c', 'English', 'This is folder to learning', NOW(), NULL, '237c736b-9aa3-4c4e-8d7e-558166bb3d6b')
+    ('21a95216-b38d-483f-bb26-cc8cbdd9366c', 'English', 'This is folder to learning', NOW(), NULL, '237c736b-9aa3-4c4e-8d7e-558166bb3d6b');
