ec9e4491a93c57f1859e44f66d66f707b7c00538
nguyenhoanganhtuan1206 <nguyenhoanganhtuan1206@gmail.com>
2025-03-24 10:16:41 +0700

    Add check expiration date of token

--- quizlet-fe/src/components/PrivateRoutes/index.tsx
+++ quizlet-fe/src/components/PrivateRoutes/index.tsx
@@ -1,24 +1,33 @@
-import { useSelector } from "react-redux";
-import { RootState } from "../../store";
-import { Navigate, Outlet, useLocation } from "react-router-dom";
-import { decodeToken } from "../../utils/jwtUtilities";
-import { JwtPayload } from "../../type/Auth/authTypes";
+import { useDispatch, useSelector } from 'react-redux';
+import { logout, RootState } from '../../store';
+import { Navigate, Outlet, useLocation } from 'react-router-dom';
+import { decodeToken } from '../../utils/jwtUtilities';
+import { JwtPayload } from '../../type/Auth/authTypes';
 
 export default function PrivateRoutes() {
   const { pathname } = useLocation();
 
+  const dispatch = useDispatch();
   const token = useSelector((state: RootState) => state.authProvider.token);
   const isAuthenticated = !!token;
   const decodedToken: JwtPayload | null = token ? decodeToken(token) : null;
 
-  if (!isAuthenticated) {
+  if (!isAuthenticated || !decodeToken) {
+    dispatch(logout());
+    return <Navigate to="/auth" replace />;
+  }
+
+  // Check token expiration
+  const currentTime = Math.floor(Date.now() / 1000);
+  if (decodedToken?.exp && currentTime >= decodedToken.exp) {
+    dispatch(logout()); // Token expired, log out
     return <Navigate to="/auth" replace />;
   }
 
   /*
    * Define the privateRoute for Admin
    */
-  if (pathname === "/admin" && decodedToken?.role !== "Admin") {
+  if (pathname === '/admin' && decodedToken?.role !== 'Admin') {
     return <Navigate to="/forbidden" replace />;
   }
 
--- quizlet-fe/src/utils/jwtUtilities.ts
+++ quizlet-fe/src/utils/jwtUtilities.ts
@@ -1,6 +1,11 @@
 import { jwtDecode } from 'jwt-decode';
 import { JwtPayload } from '../type/Auth/authTypes';
 
-export const decodeToken = (token: string): JwtPayload => {
-  return jwtDecode<JwtPayload>(token);
+export const decodeToken = (token: string): JwtPayload | null => {
+  try{ 
+    return jwtDecode<JwtPayload>(token);
+  }catch(error) {
+    console.error('Failed to decode token: ', error);
+    return null;
+  }
 };
